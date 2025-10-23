6c74fa085e17ce170b3c8656eb5b61441597fb29
alexsim2007 <aleksandrsimonov9622@gmail.com>
2025-10-23 21:58:42 +0300

    Fix

--- Startup.cs
+++ Startup.cs
@@ -112,6 +112,7 @@ namespace NoteFlow
             window.OnClosed += () =>
             {
                 Electron.App.Quit();
+                Electron.App.Exit();
             };
         }
     }
